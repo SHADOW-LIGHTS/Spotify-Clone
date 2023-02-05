335e7601e19a7816bf385bfc365dc62c8d011777
PRUTHVI <108618163+SHADOW-LIGHTS@users.noreply.github.com>
2023-02-05 12:41:41 +0530

    Update _middleware.js

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -16,7 +16,7 @@ export async function middleware(req) {
   }
 
   //redirect them to login if they dont have token AND are request a protected reVVimportimportuser
-  if (!token && pathname !== "https://www.tune-in.site/login") {
-    return NextResponse.redirect("/login");
+  if (!token && pathname !== "/login") {
+    return NextResponse.redirect("https://www.tune-in.site");
   }
 }
