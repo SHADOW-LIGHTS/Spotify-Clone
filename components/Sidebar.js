b94632b470f562bf56007bda344a23eb0d6ac4d4
PRUTHVI <108618163+SHADOW-LIGHTS@users.noreply.github.com>
2023-02-21 08:17:10 +0530

    Update Sidebar.js

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -30,7 +30,7 @@ function Sidebar() {
   }, [session, spotifyApi]);
 
   return (
-    <div className="text-gray-500 p-5 text-sm overflow-y-scroll scrollbar-hide h-screen hidden md:inline-flex shrink-0 pb-50">
+    <div className="text-gray-500 p-5 text-sm overflow-y-scroll scrollbar-hide h-screen hidden md:inline-flex shrink-0 pb-100">
       <div className="space-y-4">
         <button className="flex items-center space-x-2 hover:text-white">
           <HomeIcon className="h-5 w-5" />
