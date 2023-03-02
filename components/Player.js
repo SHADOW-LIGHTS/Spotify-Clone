afcb0b1a302147cda98cbae1dc32ad825dac42c0
PRUTHVI <108618163+SHADOW-LIGHTS@users.noreply.github.com>
2023-03-02 10:37:16 +0530

    Update Player.js

--- components/Player.js
+++ components/Player.js
@@ -79,7 +79,7 @@ function Player() {
       {/* left */}
       <div className="flex items-center space-x-4">
         <img
-          className="hidden md:inline h-15 w-15"
+          className="hidden md:inline h-11 w-11"
           src={songInfo?.album.images?.[0]?.url}
           alt=""
         />
